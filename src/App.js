5329b9ee86006e763e780b1a699d7bbee2a80ca3
ledinhcanh <lecanh2810@gmail.com>
2020-02-23 20:35:33 +0700

    tasks list

--- src/App.js
+++ src/App.js
@@ -5,7 +5,51 @@ import Control from './components/Control'
 import TaskList from './components/TaskList'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tasks: [] // id, name, status
+    }
+  }
+  UNSAFE_componentWillMount(){
+    if(localStorage && localStorage.getItem('tasks')){
+      var tasks = JSON.parse(localStorage.getItem('tasks'));
+      this.setState({
+        tasks: tasks
+      });
+    }
+  }
+  onGenerateData = () => {
+    var tasks = [
+      {
+        id: this.generateID(),
+        name: 'Hoc lap trinh',
+        status: true
+      },
+      {
+        id: this.generateID(),
+        name: 'Di boi',
+        status: false
+      },
+      {
+        id: this.generateID(),
+        name: 'Ngu',
+        status: true
+      }
+    ]
+   
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  generateID() {
+    return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + this.s4() + this.s4();
+  }
+
   render() {
+    var {tasks} = this.state;
     return (
       <div className="container">
         <div className="text-center">
@@ -14,16 +58,19 @@ class App extends Component {
         </div>
         <div className="row">
           <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-          <TaskForm />
+            <TaskForm />
           </div>
           <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
             <button type="button" className="btn btn-primary">
               <span className="fa fa-plus mr-2" />Thêm Công Việc
             </button>
+            <button type="button" className="btn btn-danger ml-2" onClick={this.onGenerateData}>
+              Generate Date
+            </button>
             <Control />
             <div className="row mt-3">
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-               <TaskList/>
+                <TaskList tasks={tasks}/>
               </div>
             </div>
           </div>
