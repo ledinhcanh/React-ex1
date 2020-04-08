03ab2d0fab1415724a9b4a7bd341879b1ece5a6b
ledinhcanh <lecanh2810@gmail.com>
2020-04-08 09:10:49 +0700

    ok

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,25 +1,94 @@
 import React, { Component } from 'react';
 
 class TaskForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: '',
+            name: '',
+            status: true
+        }
+    }
+    UNSAFE_componentWillMount() {
+        if (this.props.task) {
+            this.setState({
+                id: this.props.task.id,
+                name: this.props.task.name,
+                status: this.props.task.status
+            })
+        }
+    }
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        if (nextProps && nextProps.task) {
+            this.setState({
+                id: nextProps.task.id,
+                name: nextProps.task.name,
+                status: nextProps.task.status
+            })
+        } else if (!nextProps.task) {
+            this.setState({
+                id: '',
+                name: '',
+                status: true
+            })
+        }
+    }
+    onCloseForm = () => {
+        this.props.onCloseForm();
+    }
+    onChange = (event) => {
+        var target = event.target;
+        var name = target.name;
+        var value = target.value;
+        if (name === 'status') {
+            value = target.value === 'true' ? true : false;
+        }
+        this.setState({
+            [name]: value
+        })
+    }
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state);
+        // Cancel và close form
+        this.onClear();
+        this.onCloseForm();
+    }
+    onClear = () => {
+        this.setState({
+            name: '',
+            status: false
+        })
+    }
     render() {
+        var { id } = this.state;
         return (
             <div className="card">
-                <div className="card-header">Thêm Công Việc<span className="fa fa-times-circle float-right close"></span></div>
+                <div className="card-header">{id === '' ? 'Thêm Công Việc' : 'Cập nhật công việc'}<span className="fa fa-times-circle float-right close" onClick={this.onCloseForm}></span></div>
                 <div className="card-body">
-                    <form>
+                    <form onSubmit={this.onSubmit}>
                         <div className="form-group">
                             <label>Tên :</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control"
+                                name="name"
+                                value={this.state.name}
+                                onChange={this.onChange}
+                            />
                         </div>
                         <label>Trạng Thái :</label>
-                        <select className="form-control" required="required">
-                            <option value={1}>Kích Hoạt</option>
-                            <option value={0}>Ẩn</option>
+                        <select className="form-control" required="required"
+                            name="status"
+                            value={this.state.status}
+                            onChange={this.onChange}
+                        >
+                            <option value={true}>Kích Hoạt</option>
+                            <option value={false}>Ẩn</option>
                         </select>
                         <br />
                         <div className="text-center">
-                            <button type="submit" className="btn btn-warning">Thêm</button>&nbsp;
-              <button type="submit" className="btn btn-danger">Hủy Bỏ</button>
+                            <button type="submit" className="btn btn-warning">{id === '' ? 'Thêm' : 'Lưu lại'}</button>&nbsp;
+                            <button type="button" className="btn btn-danger" onClick={this.onClear}>Hủy Bỏ</button>
                         </div>
                     </form>
                 </div>
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class TaskItem extends Component {
+    onUpdateStatus = () => {
+        this.props.onUpdateStatus(this.props.task.id);
+    }
+    onDelete = () => {
+        this.props.onDelete(this.props.task.id);
+    }
+    onUpdate = () => {
+        this.props.onUpdate(this.props.task.id);
+    }
     render() {
         var { task, index } = this.props;
         return (
@@ -8,13 +17,13 @@ class TaskItem extends Component {
                 <td>{index}</td>
                 <td>{task.name}</td>
                 <td className="text-center">
-                    <span className={task.status === true ? 'badge badge-danger' : 'badge badge-success'}>{task.status === true ? 'Kích hoạt' : 'Ẩn'}</span>
+                    <span className={task.status === true ? 'badge badge-danger' : 'badge badge-success'} onClick={this.onUpdateStatus}>{task.status === true ? 'Kích hoạt' : 'Ẩn'}</span>
                 </td>
                 <td className="text-center">
-                    <button type="button" className="btn btn-warning">
-                        <span className="fa fa-pencil mr-2" />Sửa</button>&nbsp;
-                        <button type="button" className="btn btn-danger">
-                        <span className="fa fa-trash mr-2" />Xóa</button>
+                    <button type="button" className="btn btn-warning" onClick={this.onUpdate}>
+                        <span className="fa fa-pencil mr-2"></span>Sửa</button>&nbsp;
+                        <button type="button" className="btn btn-danger" onClick={this.onDelete}>
+                        <span className="fa fa-trash mr-2"></span>Xóa</button>
                 </td>
             </tr>
         );
